c8efb073c3328a5c5f86daca0cf723d59733bdee
AmineZalila <medamine.zalila@gmail.com>
2023-05-06 14:10:02 +0100

    commitAmine

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,10 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
+
     #[Route('/main', name: 'app_main')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    #[Route('/', name: 'app_main_2')]
+    public function index2(): Response
+    {
+        return $this->render('welcome/welcome.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
